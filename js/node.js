ad6dda5bb6e76322ee7b72bfc7f5541c645ff359
JabedH <87475551+JabedH@users.noreply.github.com>
2022-02-28 12:35:07 +0600

    add

--- js/node.js
+++ js/node.js
@@ -1,3 +1,4 @@
+// Popular Drinks
 const randomApi =() =>{
     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink')
     .then(res => res.json())
@@ -8,9 +9,7 @@ const getRandomApi = (getObjects)=>{
     const getRandomId = document.getElementById('addRandom')
     getObjects.forEach(getobj=>{
         const div = document.createElement('div')
-        div.classList.add('card')
-        div.classList.add('col-md-3')
-        div.classList.add('border-0')
+        div.classList.add('card', 'col-md-3', 'border-0')
     div.innerHTML = 
     ` <img src="${getobj.strDrinkThumb}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -21,7 +20,7 @@ const getRandomApi = (getObjects)=>{
     })
     
 }
-
+// Latest Drinks
 const nonAlcoholic =() => {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
     .then(res => res.json())
@@ -32,9 +31,7 @@ const getNonAlcoholic = (NonAlcoholicObj) =>{
     const getNonId = document.getElementById('nonAlcoholic')
     NonAlcoholicObj.forEach(nonObj =>{
         const div1 = document.createElement('div')
-        div1.classList.add('card')
-        div1.classList.add('col-md-3')
-        div1.classList.add('border-0')
+        div1.classList.add('card', 'col-md-3', 'border-0')
         div1.innerHTML = 
     ` <img src="${nonObj.strDrinkThumb}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -45,8 +42,7 @@ const getNonAlcoholic = (NonAlcoholicObj) =>{
     })
 }
 
-// input value
-
+//Search value
 const searchCocktail =() =>{
     const searchField = document.getElementById('inputValue')
     const searchValue = searchField.value
@@ -60,17 +56,20 @@ const searchCocktail =() =>{
         fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
         .then(res => res.json())
         .then(data => displayCocktail(data.drinks))
-    }
-    const displayCocktail = allCocktail =>{
+
+// clear error message and show spinner
+        error.innerText = '';
         if(onclick="searchCocktail()"){
             document.getElementById('spinner').style.display = 'block'
         }
+    }
+    const displayCocktail = allCocktail =>{
     const newCocktail = document.getElementById('newCocktail')
     newCocktail.innerHTML = ''
     allCocktail?.forEach(cocktail => {
         console.log(cocktail)
         const div2 = document.createElement('div')
-        div2.classList.add('text-center','mb-5','border-0','col-md-3','card')
+        div2.classList.add('text-center','mb-5','border-0','col-md-3','card', 'mt-3')
         div2.innerHTML = 
     ` <img src="${cocktail.strDrinkThumb}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -80,33 +79,38 @@ const searchCocktail =() =>{
     `
     newCocktail.appendChild(div2)
     if(onclick="searchCocktail()"){
-        document.getElementById('findCocktail').style.display = 'none'
-        document.getElementById('popularId').style.display = 'none'
-        document.getElementById('latestId').style.display = 'none'
+        document.getElementById('findCocktail','popularId', 'latestId').style.display = 'none'
         document.getElementById('spinner').style.display = 'none'
     }
     })
     }
 }
+
+// show details of items
 const loadCocktail = cocktailId =>{
-    console.log(cocktailId)
-    
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`)
     .then(res => res.json())
     .then(data => newLoadCocktail(data.drinks[0]))
 }
 const newLoadCocktail = cocktailDetails =>{
+    const error = document.getElementById('warningId')
     const addCocktailId = document.getElementById('cocktailNewId')
+
+//error handling and clear field
     addCocktailId.innerHTML =''
+    error.innerText ='';
+
     const div3 = document.createElement('div')
-    div3.classList.add('text-center','mb-5','border-0','col-md-4','card')
+    div3.classList.add('text-center','mb-5','mt-3','border-0','col-md-3','card')
     div3.innerHTML = 
     `<div class="card mb-3">
     <img src="${cocktailDetails.strDrinkThumb}" style="height: 12rem;" class="card-img-top" alt="...">
-    <div class="card-body">
+    <div class="c ard-body">
       <h5 class="card-title">${cocktailDetails.strDrink}</h5>
       <p class="card-text">${cocktailDetails.strInstructions.slice(0, 200)}</p>
     </div>
   </div>`
     addCocktailId.appendChild(div3)
 }
+
+// ******
